73afa87fe7bea80a1baee77da075e7deb5775743
pkyeager <phongsakon@outlook.dk>
2024-11-25 14:19:46 +0100

    Came back to SAM 1.0 since 2.0 is not fully working.

--- scripts/sam-finetune.py
+++ scripts/sam-finetune.py
@@ -1,140 +1,109 @@
-import warnings
-warnings.filterwarnings("ignore")
+'''
+Use MedSAM as inspiration to fine-tune our model for the Sentinel-1 dataset.
+'''
 
+### Setup the environment ###
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+join = os.path.join
+from tqdm import tqdm
+from skimage import transform
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import monai
 from segment_anything import sam_model_registry
-from segment_anything import *
-
-import torchvision.transforms as T
+import torch.nn.functional as F
+import argparse
+import random
+from datetime import datetime
+import shutil
+import glob
+join = os.path.join
+from tdqm import tqdm
+from skimage.io import transform
 import torch
 import torch.nn as nn
 
 
-import torch.distributed as dist
-from torch.utils.data import DataLoader, BatchSampler
-from torch.utils.data.distributed import DistributedSampler
-from torch.nn.parallel import DistributedDataParallel
+### Setup Parser ###
+# TODO: For later use, could be quite nice to have a parser for the script
+# parser = argparse.ArgumentParser()
 
-
-import os
-import argparse
-import wandb
-
-from datatime import datetime
-from torchinfo import summary
-
-CHECKPOINT_DIR = "checkpoints"
-os.makedirs(CHECKPOINT_DIR, exist_ok=True)
 
 ### Constants ###
-IMAGE_SIZE = (1024, 1024)
-NUM_CLASSES = 1
+SEED = 42
+BATCH_SIZE = 8
+NUM_EPOCHS = 20
+LEARNING_RATE = 1e-4
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DATA_DIR = "../data/sentinel-1-flipped-one-class/train"
+IMAGES_DIR = join(DATA_DIR, "images")
+MASKS_DIR = join(DATA_DIR, "masks")
+TEST_SIZE = 0.2
 
-# TODO: Can be cahnged later
-IMAGE_DIR = "data/sentinel-1-flipped-one-class/train"
-
-
-
-
-### Helper functions ###
-
-
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+### Utility Functions ###
+def show_mask(mask, ax, random_color=False):
+    color = np.array([251 / 255, 252 / 255, 30 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
 
 
-def get_args_parser():
-    '''
-    TODO: Might have to add more, but for now, this is enough
-    '''
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
-    parser.add_argument('--batch-size', type=int, default=4, help='input batch size for training (default: 4)')
-    parser.add_argument('--epochs', type=int, default=20, help='number of epochs to train (default: 20)')
-    parser.add_argument('--lr', type=float, default=0.0001, help='learning rate (default: 0.0001)')
-    parser.add_argument('--dist', type=str2bool, default=False, help='distributed training (default: False)')
-    parser.add_argument('--model_type', type=str, default='vit_t', help='SAM model type (default: vit_t)')
-    parser.add_argument('--model_checkpoint', type=str, default='sam_vit_t', help='SAM model checkpoint (feauture extractor) (default: sam_vit_t)')
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(
+        plt.Rectangle((x0, y0), w, h, edgecolor="blue", facecolor=(0, 0, 0, 0), lw=2)
+    )
 
-    return parser
+### Custom Dataset Class ###
+class SARDataset(Dataset):
+    def __init__(self, data_root, bbox_shift = 20):
 
-### Fine-tuning script ###
-def main(opts) -> str:
-    """
-    Model finetuning script
+    def __len__(self):
+        return len(self.data)
 
-    Returns:
-        str: Path to the saved models
-    """
+    def __getitem__(self, idx):
 
-    seed.set_seed(opts.seed)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Device: {device}")
+train_dataset = SARDataset(DATA_DIR)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-    ### Checkpointing ###
+### Sanity Check ###
+for step, (image, gt, bboxes, names_temp) in enumerate(tr_dataloader):
+    print(image.shape, gt.shape, bboxes.shape)
+    # show the example
+    _, axs = plt.subplots(1, 2, figsize=(25, 25))
+    idx = random.randint(0, 7)
+    axs[0].imshow(image[idx].cpu().permute(1, 2, 0).numpy())
+    show_mask(gt[idx].cpu().numpy(), axs[0])
+    show_box(bboxes[idx].numpy(), axs[0])
+    axs[0].axis("off")
+    # set title
+    axs[0].set_title(names_temp[idx])
+    idx = random.randint(0, 7)
+    axs[1].imshow(image[idx].cpu().permute(1, 2, 0).numpy())
+    show_mask(gt[idx].cpu().numpy(), axs[1])
+    show_box(bboxes[idx].numpy(), axs[1])
+    axs[1].axis("off")
+    # set title
+    axs[1].set_title(names_temp[idx])
+    # plt.show()
+    plt.subplots_adjust(wspace=0.01, hspace=0)
+    plt.savefig("../data_sanitycheck.png", bbox_inches="tight", dpi=300)
+    plt.close()
+    break
 
-    run_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_name = run_time + f'_{opts.model_type}_finetune.pth'
-    save_path = os.path.join(CHECKPOINT_DIR, file_name)
+class SARSam(nn.Module):
+    def __init__(self):
+        super().__init__()
 
+    def forward(self, x):
 
-    ### Dataset & Dataloader ###
-
-    train_transform = T.Compose([
-        T.Resize(IMAGE_SIZE[0], IMAGE_SIZE[1]),
-        T.ToImage()
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-    test_transform = T.Compose([
-        T.Resize(IMAGE_SIZE[0], IMAGE_SIZE[1]),
-        T.ToImage()
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-    train_set = dataset.make_dataset(IMAGE_DIR,
-                                     transform=train_transform)
-
-    val_set = dataset.make_dataset(IMAGE_DIR,
-                                      transform=test_transform)
+def main():
 
 
-    train_loader = DataLoader(train_set, batch_size=opts.batch_size, shuffle=True)
-    val_loader = DataLoader(val_set, batch_size=opts.batch_size, shuffle=False)
-
-    ### SAM Config ###
-    sam_checkpoint = opts.model_checkpoint
-    model_type = opts.model_type
-
-    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-    sam.to(device)
-
-    ### Freezing the model ###
-
-    for _, p in sam.image_encoder.named_parameters():
-        p.requires_grad = False
-
-    for _, p in sam.prompt_encoder.named_parameters():
-        p.requires_grad = False
-
-    for _, p in sam.mask_decoder.named_parameters():
-        p.requires_grad = True
-
-    # print model info
-
-    print()
-    print(" === MODEL INFO ===")
-    summary(sam, input_size=(opts.batch_size, 3, IMAGE_SIZE[0], IMAGE_SIZE[1]))
-    print()
-
-    model = sam
-
-    ### Loss & Optimizer ###
-    bceloss = nn.BCEWithLogitsLoss()
-    iouloss = iou_loss_torch.IoULoss().to(device)
+if __name__ == "__main__":
+    main()
